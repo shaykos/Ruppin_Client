cd5c066b28b3dded5e80fda61b23963f35af0082
Shay Abraham <shaykos@gmail.com>
2026-05-11 15:42:35 +0300

    1

---
+++ lesson 7 - intro to js/scripts/main.js
@@ -0,0 +1,20 @@
+// alert("צהריים טובים! יאללה לקום!!!!");
+
+// console.log("מה קורה??? עוד לא התעוררתם???");.
+
+// let x = "Moshe";
+// console.log(x);
+// console.log(typeof x); // typeof --> מביא את סוג המשתנה
+// x = 12; 
+// console.log(x);
+// console.log(typeof x);
+
+// let name = "Kobi";
+// const x = 12;
+// x = 5; // מתקבלת שגיאה  
+
+let n1 = Number(prompt("הזן ציון 1")); // int n1 = int.Parse(Console.ReadLine());
+let n2 = Number(prompt("הזן ציון 2"));
+let n3 = Number(prompt("הזן ציון 3"));
+
+console.log((n1 + n2 + n3) / 3);
