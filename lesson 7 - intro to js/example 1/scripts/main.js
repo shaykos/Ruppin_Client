3a7879ed446e4956428b965b4332b79cbf13e730
Shay Abraham <shaykos@gmail.com>
2026-05-11 16:32:26 +0300

    ifs and loops

--- lesson 7 - intro to js/example 1/scripts/main.js
+++ lesson 7 - intro to js/example 1/scripts/main.js
@@ -13,8 +13,9 @@
 // const x = 12;
 // x = 5; // מתקבלת שגיאה  
 
-let n1 = Number(prompt("הזן ציון 1")); // int n1 = int.Parse(Console.ReadLine());
+let n1 = Number(prompt("הזן ציון 1")); // c# --> int n1 = int.Parse(Console.ReadLine());
 let n2 = Number(prompt("הזן ציון 2"));
 let n3 = Number(prompt("הזן ציון 3"));
 
-console.log((n1 + n2 + n3) / 3);
+console.log(`the average is ${(n1 + n2 + n3) / 3}`);
+// c# --> Conswole.WriteLine($"the average is {(n1 + n2 + n3) / 3}");
---
+++ lesson 7 - intro to js/exmaple 2/scripts/main.js
@@ -0,0 +1,43 @@
+// let day = Number(prompt("enter a number between 1-7"));
+
+// if (day < 1 || day > 7) {
+//     alert("I TOLD YOU TO CHOOSE BETWEEN 1 AND 7 YOU ****");
+// }
+
+// switch (day) {
+//     case 1:
+//         console.log("Sunday");
+//         break;
+//     case 2:
+//         console.log("Monday");
+//         break;
+//     case 3:
+//         console.log("Thu");
+//         break;
+//     case 4:
+//         console.log("Wed");
+//         break;
+//     case 5:
+//         console.log("Th");
+//         break;
+//     case 6:
+//         console.log("Friday");
+//         break;
+//     case 7:
+//         console.log("Sutarday");
+//         break;
+//     default:
+//         console.log("***!!@#!#$@%@$@%&$^&%^***");
+//         break;
+
+// }
+
+
+let num = Number(prompt("הזן מספר חיובי"));
+
+debugger;
+while (num <= 0) {
+    num = Number(prompt("נסה שוב להזין מספר חיובי"));
+}
+
+console.log('shhhhhhhh!!!!');
