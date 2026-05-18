edbb22a65a03b0f9fed9c81b66a45fe0e8a8d0e2
Shay Abraham <shaykos@gmail.com>
2026-05-18 16:19:53 +0300

    array 1

---
+++ lesson 8 - advanced programming/scripts/main.js
@@ -0,0 +1,68 @@
+
+let menu = ["rice", "pizza", "ice-cream"];
+console.log('menu.length -->', menu.length);
+console.log('menu[3] --> ', menu[3]);
+menu[3] = "sushi";
+menu[100] = "avokado";
+
+console.log('menu -->', menu);
+
+
+let book = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+book.push(11);
+console.log('book after push --> ', book);
+
+let last = book.pop();
+console.log('book after pop --> ', book);
+console.log('last --> ', last);
+
+let first = book.shift();
+console.log('book after shift --> ', book);
+console.log('first --> ', first);
+
+book.unshift("BOOK COVER");
+console.log('book after unshift --> ', book);
+
+console.log('book.join() --> ', book.join());
+console.log('book.join("-->") --> ', book.join("-->"));
+
+let arr = menu.concat(book);
+console.log("arr.join() --> ", arr.join());
+
+book.reverse();
+console.log("book after reverse() --> ", book);
+
+let minibook = book.slice(4, 7);
+console.log("minibook --> ", minibook);
+
+/* 
+C# foreach
+-----------
+int[] nums = {1,2,3,5};
+foreach(int n in nums) {Console.WriteLine(n);}
+*/
+console.log("foreach");
+let nums = [1, 2, 3, 4, 5];
+nums.forEach((n) => { console.log(n) });
+/*
+    for(let i = 0; i < nums.length; i++){
+        let n = nums[i];
+        console.log(n);
+    }
+*/
+
+let prices = [100, 10, 20, 500];
+console.log("map");
+let pricesWithVAT = prices.map((p) => { return p * 1.18 });
+console.log('pricesWithVAT --> ', pricesWithVAT);
+/*
+    let prices = [100, 10, 20, 500];
+    let pricesWithVAT = [];
+    for(let i = 0; i < prices.length; i++){
+        pricesWithVAT.push(prices[i] * 1.18);
+    }
+*/
+
+console.log(pricesWithVAT[pricesWithVAT.length - 1]);
+console.log(pricesWithVAT.at(-1));
+console.log(pricesWithVAT.at(-2));
